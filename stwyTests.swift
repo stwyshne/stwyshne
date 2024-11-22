15140b18cae0c6415183521c22954cc0ebecbb80
stwyshne <k9md25@gmail.com>
2024-11-22 05:54:31 +0200

    Add files via upload

---
+++ stwyTests.swift
@@ -0,0 +1,16 @@
+//
+//  stwyTests.swift
+//  stwyTests
+//
+//  Created by KJ on 01/05/1446 AH.
+//
+
+import Testing
+
+struct stwyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
